f72711a07f85fa74dea518c6df682eaeefca5c3d
conrad-scherb <csch666@aucklanduni.ac.nz>
2022-03-19 00:44:12 +1300

    feat: setup dummy prisma integration

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,13 @@
 const Database = require("better-sqlite3");
-new Database('prisma/rekuebox.db', { verbose: console.log })
+const db = new Database('prisma/rekuebox.db', { verbose: console.log })
+
+const query = db.prepare(`CREATE TABLE contacts (
+	contact_id INTEGER PRIMARY KEY,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	email TEXT NOT NULL UNIQUE,
+	phone TEXT NOT NULL UNIQUE
+)`);
+
+query.run();
+
